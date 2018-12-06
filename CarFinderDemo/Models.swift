05aa928e43d7db2c2cf1972f13a07da5e089be13
Owen Thomas <owen@owenhthomas.com>
2018-12-05 16:07:22 -0800

    Clean up models

--- CarFinderDemo/Models.swift
+++ CarFinderDemo/Models.swift
@@ -46,19 +46,7 @@ struct Car: Decodable {
   var rates: [Rate]
   var vehicleInfo: VehicleInfo
   var estimatedTotal: Cost?
-  var image: Image?
-  
-  enum CodingKeys: String, CodingKey {
-    case rates, vehicleInfo, estimatedTotal, image
-  }
-  
-  init(from decoder: Decoder) throws {
-    let values = try decoder.container(keyedBy: CodingKeys.self)
-    rates = try values.decode([Rate].self, forKey: .rates)
-    vehicleInfo = try values.decode(VehicleInfo.self, forKey: .vehicleInfo)
-    estimatedTotal = try values.decodeIfPresent(Cost.self, forKey: .estimatedTotal)
-    image = try values.decodeIfPresent(Image.self, forKey: .image)
-  }
+  var image: Image?  
 }
 
 struct CarInfo {
@@ -78,25 +66,11 @@ struct CarInfo {
 
 struct VehicleInfo: Decodable {
   var acrissCode: String
-  var transmission: String
-  var fuel: String
+  var transmission: String = "Unknown Transmission"
+  var fuel: String = "Unknown Fuel Type"
   var airConditioning: Bool?
   var category: String
-  var type: String
-  
-  enum CodingKeys: String, CodingKey {
-    case acrissCode, transmission, fuel, airConditioning, category, type
-  }
-  
-  init(from decoder: Decoder) throws {
-    let values = try decoder.container(keyedBy: CodingKeys.self)
-    acrissCode = try values.decode(String.self, forKey: .acrissCode)
-    transmission = try values.decodeIfPresent(String.self, forKey: .transmission) ?? "Unknown Transmission"
-    fuel = try values.decodeIfPresent(String.self, forKey: .fuel) ?? "Unknown Fuel Type"
-    airConditioning = try values.decodeIfPresent(Bool.self, forKey: .airConditioning)
-    category = try values.decode(String.self, forKey: .category)
-    type = try values.decodeIfPresent(String.self, forKey: .type) ?? "Unknown Type"
-  }
+  var type: String = "Unknown Type"
 }
 
 struct Rate: Decodable {
